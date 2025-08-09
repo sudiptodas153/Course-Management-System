f4e908400416527d9b140329b64831cbd5069d39
sudiptodas153 <sudiptodas4093@gmail.com>
2025-08-09 22:21:36 +0600

    add text size

--- src/Shared/PopularCourses/PopularCourses.jsx
+++ src/Shared/PopularCourses/PopularCourses.jsx
@@ -41,7 +41,7 @@ const PopularCourses = () => {
 
                                     <div className="px-4">
                                         <Link to={`/courses/${course._id}`}>
-                                            <button className="btn border-1 text-emerald-400 border-emerald-400 hover:text-white text-lg font-semibold hover:border-none hover:bg-emerald-400 w-full">Details</button>
+                                            <button className="btn border-1 text-emerald-500 border-emerald-400 hover:text-white text-lg font-semibold hover:border-none hover:bg-emerald-400 w-full">Details</button>
                                         </Link>
                                     </div>
                                 </div>
